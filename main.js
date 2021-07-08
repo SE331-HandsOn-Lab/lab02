db35c311edff7324924526b3ea7101eb1f1cb870
pasakon <kongpazam@gmail.com>
2021-07-08 18:08:59 +0700

    3.4
    remove image and inStock in data fuction

--- main.js
+++ main.js
@@ -3,16 +3,16 @@ const app = Vue.createApp({
         return {
             product: 'Shoes',
             brand: 'SE 331',
-            image: './assets/images/socks_green.jpg',
-            inStock: false,
+            // image: './assets/images/socks_green.jpg',
+            // inStock: true,
             inventory: 100,
             details: ['50% cotton', '30% wool', '20% polyester'],
             variants: [
-                { id: 2234, color: 'green', image: './assets/images/socks_green.jpg' },
-                { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg' }
+                { id: 2234, color: 'green', image: './assets/images/socks_green.jpg',quantity: 50 },
+                { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg',quantity: 0 }
             ],
-            cart: 0
-
+            cart: 0,
+            selectedVariant: 0
         }
     },
     methods: {
@@ -21,11 +21,20 @@ const app = Vue.createApp({
         },
         updateImage(variantImage) {
             this.image = variantImage
+        },
+        updateVariant(index){
+            this.selectedVariant = index
         }
     },
     computed: {
         title(){
             return this.brand + ' ' + this.product
+        },
+        image(){
+            return this.variants[this.selectedVariant].image
+        },
+        inStock(){
+            return this.variants[this.selectedVariant].quantity
         }
     }
 })
