ebaaeaafb194a527793a1bf7ad717510f5570438
pasakon <kongpazam@gmail.com>
2021-07-08 18:43:31 +0700

    3.5
    add onsale boolean

--- main.js
+++ main.js
@@ -6,6 +6,7 @@ const app = Vue.createApp({
             // image: './assets/images/socks_green.jpg',
             // inStock: true,
             inventory: 100,
+            onSale : false,
             details: ['50% cotton', '30% wool', '20% polyester'],
             variants: [
                 { id: 2234, color: 'green', image: './assets/images/socks_green.jpg',quantity: 50 },
@@ -24,11 +25,21 @@ const app = Vue.createApp({
         },
         updateVariant(index){
             this.selectedVariant = index
+        }, 
+        updateonSale(){
+            this.onSale =!this.onSale
+            console.log(this.onSale)
         }
     },
     computed: {
         title(){
             return this.brand + ' ' + this.product
+            // if(this.variants[this.selectedVariant].quantity>0){
+            //     return this.brand + ' ' + this.product+' is on sale'
+            // }
+        },
+        titleonsale(){
+            return this.brand + ' ' + this.product+' is on sale'
         },
         image(){
             return this.variants[this.selectedVariant].image
